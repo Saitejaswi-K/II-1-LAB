1fe0656ee4cfbc9b8447d7ca5396e47a0f45ceab
Saitejaswi-K <93609579+Saitejaswi-K@users.noreply.github.com>
2022-01-09 14:02:53 +0530

    Linear search without recursion

---
+++ DS LAB/EXPERIMENT 4/linear_search[1].c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int linear_search(int[],int,int);
+ 
+int main(){
+    int i, a[20],key, n, p;
+    printf("enter the size of an array:");
+    scanf("%d",&n);
+    printf("enter the elements in an array:");
+    for(i=0;i<n;i++){
+       scanf("%d",&a[i]);
+    }
+    printf("element to be searched:");
+    scanf("%d",&key);
+    p = linear_search(a,n,key);
+    if(p>=0){
+       printf("element found at %d",p+1);
+    }
+    else{
+       printf("element not found");
+    }
+    printf("\n");
+    return 0;
+}
+
+int linear_search(int a[20],int n,int key){
+    int i;
+    for(i=0;i<n;i++){
+           if(key == a[i]){
+                return i;
+           } 
+    }
+    return -1;
+}
